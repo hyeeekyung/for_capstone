72808f73952853c3b8955947ce05f26d9fbe7b75
hyeeekyung <hhgyeong87@gmail.com>
2025-05-21 02:26:34 +0900

    Delete

--- capstone_backup_3/tools/S3Scanner/.dev/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apk add curl ca-certificates
-http_proxy=mitmproxy:8080 curl http://mitm.it/cert/pem -o /usr/local/share/ca-certificates/mitmproxy-ca-cert.pem
-update-ca-certificates
-tail -f /dev/null
